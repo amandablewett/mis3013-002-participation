35e1d4d2abee2c0712d1dd761639b8fc5937580c
Amanda Blewett <amblewett01@ou.edu>
2021-02-10 13:03:30 -0600

    Completed In Class Fizz Buzz example.

---
+++ InClassExamples/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string developer = "Amanda Blewett";
+            Random rand = new Random();
+            int randomNum = rand.Next(1, 101);
+
+            if (randomNum % 3 == 0 && randomNum % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+
+            else if (randomNum % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            else if (randomNum % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            else
+            {
+                Console.WriteLine(randomNum);
+            }
+
+            Console.WriteLine($"Developed by {developer}");
+        }
+    }
+}
