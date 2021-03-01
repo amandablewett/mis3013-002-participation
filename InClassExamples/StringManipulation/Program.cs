53c32f92dc7cb672e5387677b77d58e3970f15a9
Amanda Blewett <amblewett01@ou.edu>
2021-03-01 13:09:18 -0600

    Finished the in class string manipulation and output the artists names looking normally.

---
+++ InClassExamples/StringManipulation/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "tAylOr SwIFT, tHe WeEknD, TRaVis sCoTt, aRiANa gRAndE, bIlLiE eiLiSh";
+            Console.WriteLine(message);
+
+            //string newMess = message.ToUpper()[0] + message.Substring(1).ToLower();
+
+            string[] artists = message.Split(',');
+            for (int i = 0; i < artists.Length; i++)
+            {
+                string artist = artists[i].Trim();
+
+                string[] names = artist.Split(' ');
+                string first = names[0];
+                string last = names[1];
+
+                string newArtist= first.ToUpper()[0] + first.Substring(1).ToLower() + " " + last.ToUpper()[0] + last.Substring(1).ToLower();
+                Console.WriteLine(newArtist);
+                
+            }
+
+
+        }
+    }
+}
