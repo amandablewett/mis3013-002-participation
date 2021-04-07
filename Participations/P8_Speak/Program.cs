6f407a68c3fdc751ef385fac87e9179244aecbc9
Amanda Blewett <amblewett01@ou.edu>
2021-04-07 12:50:06 -0500

    Finished the Simple Method Speak project participation.

---
+++ Participations/P8_Speak/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P9_Speak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What type of animal do you want?");
+            string animal = Console.ReadLine().ToLower();
+            Console.WriteLine($"A {animal} makes the sound {speak(animal)}!");
+        }
+
+        static String speak(string animal)
+        {
+            Dictionary<string, string> sounds = new Dictionary<string, string>();
+            sounds.Add("dog", "Bark");
+            sounds.Add("cat", "Meow");
+            sounds.Add("bear", "Grrr");
+            sounds.Add("snake", "Sssss");
+            sounds.Add("lion", "Roar");
+            sounds.Add("mouse", "Squeak");
+            if (sounds.ContainsKey(animal))
+            {
+                return sounds[animal];
+            }
+            return "BOOYAH";
+        }
+    }
+}
