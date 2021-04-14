cc2ad894ae5eef18eb322fc68cd92d047a001194
Amanda Blewett <amblewett01@ou.edu>
2021-04-14 13:39:59 -0500

    Finished P10 Classes-Toy

---
+++ Participations/P.10_Classes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P10_ClassToy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Toy> toys = new List<Toy>();
+            Toy t1 = new Toy("Build-A-Bear", "Stuffed Animal", 30.99, "Brown Cat");
+            Toy t2 = new Toy("Mattel", "Barbie Doll", 12.89, "Purple fairy from Rainbow Magic Movie");
+            Toy t3 = new Toy();
+            toys.Add(t1);
+            toys.Add(t2);
+            toys.Add(t3);
+
+            foreach(Toy t in toys)
+            {
+                Console.WriteLine($"{t}\nAisle: {t.GetAisle()}");
+
+            }
+
+        }
+    }
+}
---
+++ Participations/P.10_Classes/Toy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P10_ClassToy
+{
+    class Toy
+    {
+        
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes { get; set; }
+        public Toy()
+        {
+            Manufacturer = "Ibeco Toys";
+            Name = "Bouncy Balls";
+            Price = 5.99;
+            Notes = "Comes with 6 balls";
+        }
+        public Toy (string manu, string name, double price, string notes)
+        {
+            Manufacturer = manu;
+            Name = name;
+            Price = price;
+            Notes = notes;
+        }
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            return Manufacturer[0] + "" + rand.Next(1,24);
+
+        }
+        public override string ToString()
+        {
+            return $"Manufacturer: {Manufacturer}\tName: {Name}\tPrice: {Price}\tNotes: {Notes}";
+        }
+    }
+}
