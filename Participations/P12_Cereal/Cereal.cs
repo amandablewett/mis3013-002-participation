365f82f5da98ab2e50a2b3866c9e3386f5e7873e
Amanda Blewett <amblewett01@ou.edu>
2021-04-21 13:29:27 -0500

    Finished Cereals

---
+++ Participations/P12_Cereal/Cereal.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P12_Cereal
+{
+    class Cereal
+    {
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Cups { get; set; }
+        public Cereal()
+        {
+            Manufacturer = "";
+            Name = "";
+            Calories = 0;
+            Cups = 0;
+        }
+        public Cereal(string manu, string name, double cal, double cup)
+        {
+            Manufacturer = manu;
+            Name = name;
+            Calories = cal;
+            Cups = cup;
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\tManufacturer: {Manufacturer}\tCalories: {Calories}\tCups: {Cups}";
+        }
+    }
+}
---
+++ Participations/P12_Cereal/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace P12_Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Cereal_Data.txt");
+            Cereal c;
+            List<Cereal> cereals = new List<Cereal>();
+            string[] sp;
+            for(int x = 1; x < lines.Length;x++)
+            {
+                sp = lines[x].Split("|");
+                c = new Cereal(sp[1],sp[0],Convert.ToDouble(sp[2]),Convert.ToDouble(sp[3]));
+                cereals.Add(c);
+            }
+            Console.WriteLine("\nCereals with a serving size >= 1 cup:\n");
+            foreach(Cereal cereal in cereals)
+            {
+                if (cereal.Cups >= 1)
+                {
+                    Console.WriteLine(cereal);
+                }
+            }
+            Console.WriteLine("\nCereals with calories <= 100:\n");
+            foreach (Cereal cereal in cereals)
+            {
+                if (cereal.Calories <= 100)
+                {
+                    Console.WriteLine(cereal);
+                }
+            }
+        }
+    }
+}
